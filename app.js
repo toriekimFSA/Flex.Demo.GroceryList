fa9c7a1e7b80d0b2c4f0768e0b5bb0d6b4d4467f
Torie Kim <torie.kim@fullstackacademy.com>
2023-10-26 16:25:20 -0400

    add solution

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const state = {
     { name: "apple", price: 1.75, quantity: 1 },
     { name: "banana", price: 0.25, quantity: 1 },
   ],
-  complete: [],
+  quantity: 0,
+  price: 0,
 };
 
 const items = [
@@ -22,8 +23,109 @@ const items = [
   { name: "rice", price: 5 },
 ];
 
+// ******* REFERENCES *******
+const form = document.querySelector("form");
+const groceryInput = document.getElementById("grocery-input");
+const clearButton = document.getElementsByClassName("clear-btn")[0];
+
+const tableBody = document.getElementById("table-body");
+const totalQty = document.getElementById("total-qty");
+const totalPrice = document.getElementById("total-price");
+
+// const addItemIntervalId = setInterval(addGroceryItem, 3000);
+
+// Initial Render
+render();
+
 // ******* EVENT LISTENERS *******
+// Submit to add item to grocery list
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const itemValue = groceryInput.value;
+  const item = items.find((item) => item.name === itemValue.toLowerCase());
+  const qty = Math.floor(Math.random() * 5) + 1;
+  const price = (Math.random() * 10).toFixed();
+
+  if (item) {
+    state.groceries.push({
+      name: item.name,
+      quantity: qty,
+      price: item.price * qty,
+    });
+  } else {
+    state.groceries.push({
+      name: itemValue,
+      quantity: qty,
+      price,
+    });
+  }
+
+  groceryInput.value = "";
+  render();
+});
+
+// Clear all items from grocery list
+clearButton.addEventListener("click", (event) => {
+  state.groceries = [];
+  render();
+});
 
 // ******* RENDER *******
+function render() {
+  const itemRows = state.groceries.map((item) => {
+    const newRow = document.createElement("tr");
+
+    const name = document.createElement("td");
+    name.innerText = item.name;
+
+    const quantity = document.createElement("td");
+    quantity.innerText = item.quantity;
+
+    const price = document.createElement("td");
+    price.innerText = `$${item.quantity * item.price}`;
+
+    newRow.replaceChildren(name, quantity, price);
+    return newRow;
+  });
+
+  tableBody.replaceChildren(...itemRows);
+
+  calculateTotalQty();
+  calculateTotalPrice();
+
+  totalQty.innerText = state.quantity;
+  totalPrice.innerText = `$${state.price}`;
+}
 
 // ******* FUNCTIONS *******
+function calculateTotalQty() {
+  const qty = state.groceries.reduce((accum, item) => accum + item.quantity, 0);
+
+  state.quantity = qty;
+
+  return qty;
+}
+
+function calculateTotalPrice() {
+  const totalPrice = state.groceries
+    .reduce((accum, item) => accum + item.quantity * item.price, 0)
+    .toFixed(2);
+
+  state.price = totalPrice;
+
+  return totalPrice;
+}
+
+// function addGroceryItem() {
+//   const item = items[Math.floor(Math.random() * items.length)];
+//   const quantity = Math.floor(Math.random() * 10) + 1;
+
+//   state.groceries.push({ ...item, quantity });
+
+//   render();
+
+//   if (state.groceries.length === 10) {
+//     clearInterval(addItemIntervalId);
+//   }
+// }
